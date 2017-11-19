9f9b05c9f6154d6712918425ae242ce0f9b70ea4
DESKTOP-39ABF47\Hong Zheng <zh_1991@163.com>
2017-11-19 17:26:31 +0800

    add mirror api, and add some package name

--- src/annotations/PasswordUtils.java
+++ src/annotations/PasswordUtils.java
@@ -1,3 +1,5 @@
+package annotations;
+
 //: annotations/PasswordUtils.java
 import java.util.*;
 
@@ -7,7 +9,7 @@ public class PasswordUtils {
   public boolean validatePassword(String password) {
     return (password.matches("\\w*\\d\\w*"));
   }
-  @UseCase(id = 48)
+  @UseCase(id = 48, description ="")
   public String encryptPassword(String password) {
    return new StringBuilder(password).reverse().toString();
   }
